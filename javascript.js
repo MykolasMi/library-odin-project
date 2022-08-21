435f146a43a2b23d47e1da5fdb5147f1b2689e4c
MykolasMi <mykolas.miskinisss@gmail.com>
2022-08-21 22:50:03 +0300

    Add html and header and content with form, add javascript file with skeleton code

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('form');
+
+
+
+let library = [];
+
+function book() {
+
+}
+
+function addBook () {
+    console.log('bruh')
+}
+
+form.addEventListener('submit', function() {
+    addBook()
+})
