cc6d40b2aff803e7fc66d32e1551c9ec12daea9c
MykolasMi <mykolas.miskinisss@gmail.com>
2022-08-22 00:56:38 +0300

    add functions to js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,25 @@
 const form = document.getElementById('form');
 
-
+const bookName = document.getElementById('name');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('read');
 
 let library = [];
 
-function book() {
+function book(bookName, author, pages, read) {
+    this.bookName = bookName
+    this.author = author
+    this.pages = pages
+    this. read = read
 
+    this.conCat(library);
 }
 
 function addBook () {
-    console.log('bruh')
+    console.log('addBook');
+
+    new book(bookName, author, pages, read)
 }
 
 form.addEventListener('submit', function() {
