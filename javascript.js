04306fae97a2349029ae16b720d44b9a25e92e97
MykolasMi <mykolas.miskinisss@gmail.com>
2022-09-05 23:45:31 +0300

    add fully functioning functions for adding and displaying books

--- javascript.js
+++ javascript.js
@@ -1,27 +1,70 @@
-const form = document.getElementById('form');
-
 const bookName = document.getElementById('name');
 const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const read = document.getElementById('read');
 
-let library = [];
+const submitButton = document.getElementById('submitButton');
 
-function book(bookName, author, pages, read) {
-    this.bookName = bookName
-    this.author = author
-    this.pages = pages
-    this. read = read
+const form = document.getElementById('form');
+const tableRow = document.getElementById('tableRow');
 
-    this.conCat(library);
+let library = [
+    {
+        bookName: 'The Hunger Games',
+        author: 'Suzanne Collins',
+        pages: '374',
+        read: true
+    }
+];
+
+function newBook(bookName, author, pages, read) {
+    this.bookName = bookName.value;
+    this.author = author.value;
+    this.pages = pages.value;
+    this.read = read.checked;
+
+    console.log("f.book");
 }
 
-function addBook () {
-    console.log('addBook');
+function addBookToLibrary () {
+    let book = new newBook(bookName, author, pages, read);
+    library.push(book);
+    displayBooks();
 
-    new book(bookName, author, pages, read)
+    console.log("f.addBookToLibrary");
+    console.log(book);
 }
 
-form.addEventListener('submit', function() {
-    addBook()
+//book.prototype = Object.create(addBookToLibrary.prototype)
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    //let book = new newBook(bookName, author, pages, read);
+    addBookToLibrary();
+
+    console.log("submitButton")
 })
+
+let i = 0;
+function displayBooks () {
+    for(i; i<library.length; i++) {
+        const cBookName = document.createElement('td');
+        const cAuthor = document.createElement('td');
+        const cPages = document.createElement('td');
+        const cRead = document.createElement('td');
+
+        //card.classList.add('STYLING');
+        cBookName.textContent = library[i].bookName;
+        cAuthor.textContent = library[i].author;
+        cPages.textContent = library[i].pages;
+        cRead.textContent = library[i].read;
+        
+        tableRow.appendChild(cBookName);
+        tableRow.appendChild(cAuthor);
+        tableRow.appendChild(cPages);
+        tableRow.appendChild(cRead);
+
+        console.log('loop');
+    }
+    console.log('f.displayBooks')
+}
